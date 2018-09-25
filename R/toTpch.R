2c9f5d9534719a8c8604ad6fd220d000c43a7875
Göran Broström <goran.brostrom@umu.se>
2018-09-25 17:48:18 +0200

    add pch via glm

---
+++ R/toTpch.R
@@ -0,0 +1,29 @@
+#' Transform a "survival" data frame Surv(enter, exit, event) to tabular form
+#' with 'event' and 'exposure' and aggregating
+#' 
+#' @param dat The survival data frame.
+#' @param cuts Vector defining the age periods of constant hazard.
+#' 
+#' @export toTpch
+toTpch <- function(dat, cuts){
+    ## 'dat' is typically 'vb1' or 'vb2'.
+    n <- length(cuts) - 1
+    out <- age.window(dat, cuts[1:2])
+    out$age <- 1
+    for (i in 2:n){
+        tmp <- age.window(dat, cuts[i:(i+1)])
+        tmp$age <- i
+        out <- rbind(out, tmp)
+    }
+    
+    out$exposure <- out$exit - out$enter
+    out$enter <- out$exit <- NULL
+    covars <- dat[, -(which(names(dat) %in% c("event", "exposure")))]
+    outTab <- aggregate(out[, c("event", "exposure")], by = covars, FUN = sum)
+    ageNames <- character(n)
+    for (i in 1:n){
+        ageNames[i] <- paste(cuts[i], cuts[i+1], sep = "-")
+    }
+    outTab$age <- factor(outTab$age, labels = ageNames)
+    outTab
+}
---
+++ R/tpchreg.R
@@ -0,0 +1,53 @@
+tpchreg <- function(formula, cuts, data){
+    ## 'tpch' stands for "Tabular Piecewise Constant Hazard Regression" 
+    ## form: event ~ offset(log(exposure)) + age + ....
+    ##
+    ## cuts: Age intervals
+    ##
+    ## data: is a data frame containing:
+    ##
+    ## event: Number of events
+    ## exposure: Exposure time
+    ## age: Factor of time intervals.
+    ## The rest: Covariates
+    ## Note:
+    ## length(cuts) = length(levels(age)) + 1! (cf: ppch)
+
+    ##form <- as.formula(form)
+
+    if (length(cuts) != length(levels(data$age)) + 1) stop("cuts/age length mismatch")
+    fit <- glm(formula, data = data, family = poisson)
+    cat(fit$var)
+    x <- fit$coefficients
+    ages <- which(substr(names(x), 1, 3) == "age")
+    n <- length(ages) + 1
+    hazards <- numeric(n)
+    hazards[1] <- fit$coefficients[1]
+    if (n > 1){
+        hazards[2:n] <- fit$coefficients[ages] + hazards[1]
+    }
+    hazards <- exp(hazards)
+    ages <- c(1, ages)
+    coefficients <- fit$coefficients[-ages]
+    out <- list()
+    out$coefficients <- coefficients
+    out$hazards <- hazards
+    out$residuals <- fit$residuals
+    out$cuts <- cuts
+    out$loglik <- logLik(fit)
+    out$terms <- fit$terms
+    out$aic <- extractAIC(fit)
+    out$call <- fit$call
+    out$pfixed = FALSE
+    class(out) <- "tpchreg"
+    out$call <- match.call()
+    out
+}
+
+extractAIC.tpchreg <- function(x, scale, k = 2, ...){
+    x$aic
+}
+
+nobs.tpchreg <- function(x, ...){
+    length(x$residuals)
+}
