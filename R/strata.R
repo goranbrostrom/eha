477bd6bec63c02d3825c70e3b8c9ff7308a2c136
goranbrostrom <goran.brostrom@umu.se>
2020-11-02 08:40:14 +0100

    first commit

---
+++ R/strata.R
@@ -0,0 +1,10 @@
+#' \code{strata} function imported from \code{survival}
+#'
+#' This function is imported from the \code{survival} package. See
+#' \code{\link[survival]{strata}}.
+#'
+#' @importFrom survival strata
+#' @name import_strata
+#' @aliases strata
+#' @export strata
+NULL
