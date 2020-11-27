8b39aef35078b69c5db0894e8de1dec6b475c124
goranbrostrom <goran.brostrom@umu.se>
2020-11-27 17:45:21 +0100

    first commit

---
+++ R/hazards.R
@@ -0,0 +1,13 @@
+#' Get baseline hazards atoms from a \code{coxreg} fit
+#' 
+#' @param x A \code{coxreg} object.
+#' @return A list where each component is a two-column matrix representing
+#' hazard atoms from one stratum. The first column is event time, and the 
+#' second column is the corresponding hazard atom.
+#' @export
+hazards <- function(x){
+    ## x is output from 'coxreg'.
+    haz <- getHaz(x$y, strats = x$stratum, score = exp(x$linear.predictors))
+    names(haz) <- fit$strata
+    haz
+}
