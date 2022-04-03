8180fc17723d28d17ce52ceb0bf4e54adba89cda
Göran Broström <goran.brostrom@umu.se>
2022-04-03 12:27:40 +0200

    first commit

---
+++ R/logLik.R
@@ -0,0 +1,23 @@
+##
+#        logLik methods
+##
+logLik.coxreg <- function(object, ...){
+    if (object$nullModel){
+        out <- object$loglik[1]
+        attr(out, "df") <- 0
+    }else{
+        out <- object$loglik[2]
+        attr(out, "df") <- object$df
+    }
+    attr(out, "nobs") <- object$n.events
+    class(out) <- "logLik"
+    out
+}
+
+logLik.phreg <- function(object, ...){
+    out <- object$loglik[2]
+    dd <- diag(object$var)
+    attr(out, "df") <- sum(!is.na(dd) & dd > 0) #Stolen from logLik.survreg
+    class(out) <- "logLik"
+    out
+}
